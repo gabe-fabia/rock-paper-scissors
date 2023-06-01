eab54ea37161516da3983c0d99a508d38789b7b5
Gabe Fabia <52291257+gabe-fabia@users.noreply.github.com>
2023-06-01 11:12:13 -0400

    finished spaghetti code for rock paper scissors

--- game.js
+++ game.js
@@ -52,9 +52,49 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(computerSelection)
-console.log(playRound(playerSelection, computerSelection));
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log(computerSelection)
+// console.log(playRound(playerSelection, computerSelection));
 
 // prompt() for user input
+
+// let text = "...win..."
+// text.search(string) returns the index(poisition of the first match)
+//      or -1 otherwise.
+
+function game() {
+
+    let playerWins = 0
+    let computerWins = 0
+    let ties = 0
+
+    for (let i = 0; i < 5; i++) {
+        let computerSelection = getComputerChoice()
+        // console.log(computerSelection)
+        let playerSelection = prompt("Please enter Rock, Paper, or Scissors: ")
+
+        let round = playRound(playerSelection, computerSelection)
+
+        if (round.search("tie") !== -1) {
+            console.log("It's a tie this round!")
+            ties++
+        }
+
+        else if (round.search("win") !== -1) {
+            console.log("Player wins this round!")
+            playerWins++
+        }
+
+        else {
+            console.log("Computer wins this round!")
+            computerWins++
+        }
+    }
+
+    console.log("Player Wins: " + playerWins)
+    console.log("Computer Wins: " + computerWins)
+    console.log("Ties: " + ties)
+}
+
+game()
